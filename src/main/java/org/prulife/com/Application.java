f96ceea883ec62791438173e52ff8a647d1a1375
mhackyu <markpaderes0932@yahoo.com>
2018-10-16 14:58:21 +0800

    H2 version of flowable bpm

--- src/main/java/org/prulife/com/Application.java
+++ src/main/java/org/prulife/com/Application.java
@@ -1,20 +1,35 @@
 package org.prulife.com;
 
+import org.prulife.com.entities.Users;
+import org.prulife.com.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication()
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private UsersService usersService;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
+    @Override
+    public void run(String... args) throws Exception {
+        usersService.create("mhackyu");
+        System.out.println("Total number of Users: " + usersService.count());
+    }
+
+    //    @Bean
 //    public WebMvcConfigurer corsConfigurer() {
 //        return new WebMvcConfigurerAdapter() {
 //            @Override

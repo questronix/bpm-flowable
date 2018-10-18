aa0d76d0c12b6d905eac15bded941df30bb7b1dd
Ken Crucillo <kendrick004@gmail.com>
2018-10-18 16:13:02 +0800

    create dummy user there is no default user

--- src/main/java/org/prulife/com/Application.java
+++ src/main/java/org/prulife/com/Application.java
@@ -1,19 +1,12 @@
 package org.prulife.com;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.prulife.com.services.UsersService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
 
-
-//@Configuration
-////@ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
@@ -22,16 +15,15 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurerAdapter() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/process").allowedOrigins("http://localhost:9000");
-//                registry.addMapping("/tasks").allowedOrigins("http://localhost:9000");
-//                registry.addMapping("/policy").allowedOrigins("http://localhost:9000");
-//            }
-//        };
-//    }
+    @Bean
+    public CommandLineRunner init(final UsersService myService) {
+
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... strings) throws Exception {
+                myService.createDemoUsers();
+            }
+        };
+    }
 
 }
--- src/main/java/org/prulife/com/services/UsersService.java
+++ src/main/java/org/prulife/com/services/UsersService.java
@@ -17,4 +17,32 @@ public class UsersService {
         return usersRepository.findById(id).orElseThrow(() -> new RuntimeException("Can't find user id: " + id));
     }
 
+    public void createDemoUsers() {
+        if(usersRepository.count()<1){
+            usersRepository.save(createUser("admin", "Admin", "Admin", "0", "ADMIN"));
+
+            usersRepository.save(createUser("kenster", "Kenster", "Crucillo", "1", "CSA"));
+            usersRepository.save(createUser("mhackyu", "Mark", "Paderes", "2", "CSA"));
+            usersRepository.save(createUser("jerome", "Jerome", "Patiga", "3", "PROCESSOR"));
+            usersRepository.save(createUser("band", "Ban", "Daggao", "4", "PROCESSOR"));
+            usersRepository.save(createUser("michaely", "Michael", "Yap", "5", "PROCESSOR"));
+            usersRepository.save(createUser("socpan", "Soc", "Pandaraoan", "6", "CSA"));
+
+            usersRepository.save(createUser("csa1", "CSA1", "TEST", "7", "CSA"));
+            usersRepository.save(createUser("csa2", "CSA2", "TEST", "8", "CSA"));
+            usersRepository.save(createUser("processor1", "PROCESSOR1", "TEST", "9", "PROCESSOR"));
+            usersRepository.save(createUser("processor2", "PROCESSOR2", "TEST", "10", "PROCESSOR"));
+        }
+    }
+
+    private Users createUser(String username, String fn, String ln, String ldapid, String role){
+        Users user = new Users();
+        user.setFirstName(fn);
+        user.setLastName(ln);
+        user.setLdapId(ldapid);
+        user.setStatus(1);
+        user.setUsername(username);
+        user.setRole(role);
+        return user;
+    }
 }
